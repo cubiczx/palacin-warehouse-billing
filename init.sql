afea0898a69895315598ffd2f013116ce04b524a
Xavier Palacín Ayuso <cubiczx@hotmail.com>
2026-09-01 11:58:49 +0200

    feat(db): Add Clientes table and update Movimientos table

--- init.sql
+++ init.sql
@@ -3,14 +3,25 @@ GO
 USE Almacen;
 GO
 
-CREATE TABLE Movimientos (
-    CodCliente VARCHAR(10),
-    Estanteria VARCHAR(20),
-    Cd INT,
-    Fecha DATE
+-- Tabla Maestra para tarifas y nombres de cliente (Catálogo)
+CREATE TABLE Clientes (
+    CodCliente VARCHAR(10) PRIMARY KEY,
+    Nombre VARCHAR(100) NOT NULL,
+    Tarifa DECIMAL(10, 2) NOT NULL
 );
-GO
 
--- Índice agrupado imprescindible para que las consultas históricas sobre millones de filas sean instantáneas
+-- Insertamos los dos clientes iniciales que indica el enunciado
+INSERT INTO Clientes (CodCliente, Nombre, Tarifa) VALUES 
+('C1', 'MUEBLES MARTINEZ', 1.00),
+('C2', 'MB JUGUETES', 0.50);
+
+-- Tabla Transaccional ultra ligera (millones de registros)
+CREATE TABLE Movimientos (
+    CodCliente VARCHAR(10) NOT NULL,
+    Estanteria VARCHAR(20) NOT NULL,
+    Cd INT NOT NULL,
+    Fecha DATE NOT NULL
+);
+
 CREATE CLUSTERED INDEX IX_Movimientos_Agrupacion ON Movimientos(CodCliente, Fecha);
 GO
